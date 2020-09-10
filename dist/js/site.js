451d526d2ba9e1f080a68213cd318c4b2bbfbc4d
keerthanaAnandan <keerthanaanandanak@gmail.com>
2020-09-10 12:14:44 +0530

    updated changes

--- dist/js/site.js
+++ dist/js/site.js
@@ -39,7 +39,7 @@ window.onload = function () {
   show(question_count);
 };
 
-function next() {
+function next(e) {
   // if the question is last then redirect to final page
   if (question_count == questions.length - 1) {
     sessionStorage.setItem("time", time);
@@ -48,16 +48,20 @@ function next() {
   }
   console.log(question_count);
 
-  let user_answer = document.querySelector("li.option.active").innerHTML;
+  let user_answer = document.querySelector(".option.active").innerHTML;
+
   // check if the answer is right or wrong
   if (user_answer == questions[question_count].answer) {
     points += 10;
+
     sessionStorage.setItem("points", points);
+    //let coloranswer = document.querySelector("li.option.active");
+    //e.target.coloranswer.classList.add("correct");
   }
-  console.log(points);
 
   question_count++;
   show(question_count);
+  toggleActive();
 
   //Update the progress Bar
 
@@ -66,7 +70,6 @@ function next() {
   progressBarFull.style.width = ` ${
     (question_count / Max_Questions) * 100 + 10
   }%`;
-  console.log(progressBarFull.style.width);
 
   //progress text
   const progressText = document.getElementById("progressText");
@@ -81,17 +84,18 @@ function show(count) {
   question.innerHTML = `
   <h2>Q${count + 1}. ${questions[count].question}</h2>
    <ul class="option_group">
-  <li class="option">${first}</li>
-  <li class="option">${second}</li>
-  <li class="option">${third}</li>
-  <li class="option">${fourth}</li>
+  <li><span class="choice-prefix">A</span><span class="option">${first}</span></li>
+  <li ><span class="choice-prefix">B</span><span class="option">${second}</span></li>
+  <li ><span class="choice-prefix">C</span><span class="option">${third}</span></li>
+  <li ><span class="choice-prefix">D</span><span class="option">${fourth}</span></li>
 </ul> 
   `;
   toggleActive();
 }
 
 function toggleActive() {
-  let option = document.querySelectorAll("li.option");
+  let option = document.querySelectorAll(".option");
+
   for (let i = 0; i < option.length; i++) {
     option[i].onclick = function () {
       for (let i = 0; i < option.length; i++) {
